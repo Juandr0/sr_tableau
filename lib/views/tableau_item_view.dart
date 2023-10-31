50b0f2c698306bd63aa44706c083b08d2087cd93
Alexander <alexander.cg.carlsson@gmail.com>
2023-10-31 20:17:50 +0100

    added starting and ending time

--- lib/views/tableau_item_view.dart
+++ lib/views/tableau_item_view.dart
@@ -8,6 +8,12 @@ class TableauItemView extends StatelessWidget {
 
   @override
   Widget build(context) {
+    final String formattedStartTime =
+        formatTimeFromTimestampString(tableau.startTime);
+
+    final String formattedEndTime =
+        formatTimeFromTimestampString(tableau.endtime);
+
     return Card(
       color: const Color.fromARGB(255, 239, 239, 239),
       child: Padding(
@@ -29,9 +35,9 @@ class TableauItemView extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Text(tableau.startTime),
+              Text(formattedStartTime), // Display formatted start time
               const Spacer(), // implementera progressbar?
-              const Text("tid slut"),
+              Text(formattedEndTime),
               Image.network(
                 tableau.imageString,
                 width: 40,
@@ -42,5 +48,21 @@ class TableauItemView extends StatelessWidget {
         ]),
       ),
     );
+  }
+
+  String formatTimeFromTimestampString(String timestampString) {
+    final int startIndex = timestampString.indexOf('(') + 1;
+    final int endIndex = timestampString.indexOf(')');
+    if (startIndex >= 0 && endIndex >= 0) {
+      final String timestampValue =
+          timestampString.substring(startIndex, endIndex);
+      final int timestamp = int.parse(timestampValue);
+      final DateTime date =
+          DateTime.fromMillisecondsSinceEpoch(timestamp, isUtc: true);
+      final String formattedTime =
+          '${date.hour.toString().padLeft(2, '0')}:${date.minute.toString().padLeft(2, '0')}';
+      return formattedTime;
+    }
+    return '';
   }
 }
